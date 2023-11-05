86485a40f329d60b8bf4d3149c228d0786e96b65
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-11-05 10:15:31 +0000

    Update: formatting corrected

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
-''' Test request to parse API's
-'''
-
+"""
+getting data from the api
+"""
 
 import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1].isdigit():
-        api_endpoint = "https://jsonplaceholder.typicode.com"
-        user_id = sys.argv[1]
-        user_data = requests.get(api_endpoint + "/users/" + user_id).json()
-        employee_name = user_data.get('name')
-        todo_data = \
-            requests.get(api_endpoint + "/users/" + user_id + "/todos").\
-            json()
-        total_num_of_tasks = len(todo_data)
-        completed_tasks = [todo for todo in todo_data
-                           if todo['completed'] is True]
-        num_of_done_tasks = len(completed_tasks)
-        line_one = "Employee {} is done with tasks({}/{}):".format(
-            employee_name,
-            num_of_done_tasks,
-            total_num_of_tasks)
-        print(line_one)
-        for task in completed_tasks:
-            print("\t {}".format(task["title"]))
+    employee_Id = int(sys.argv[1])
+
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    user_data_url = "https://jsonplaceholder.typicode.com/users"
+
+    user_response = requests.get(user_data_url)
+    todo_response = requests.get(todo_url)
+
+    # if todo_response.status_code & user_response.status_code == 200:
+    todos = todo_response.json()
+    users = user_response.json()
+
+    for user in users:
+        if user.get("id") == employee_Id:
+            employee_name = user.get("name")
+
+    # filter completed tasks
+    done = []
+    total = 0
+    completed = 0
+    for todo in todos:
+        if todo.get("userId") == employee_Id:
+            total += 1
+            if todo.get("completed"):
+                completed += 1
+                done.append(todo.get("title"))
+
+    # Display the progress information
+    print(f"Employee {employee_name} is done with tasks({completed}/{total}):")
+    for _ in done:
+        print(f"\t {_}")
